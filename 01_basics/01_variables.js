27b8d9c8bda53e7832d6d7fc8bf7ccd258a7ec46
urmicky112 <singhsushant112@gmail.com>
2025-03-18 07:07:01 +0000

    datypes in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accoountEmail="hitesh@google"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+// accountId=2 // not allowed
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in blcok scope and functional scope
+*/
+console.table([accountId,accoountEmail,accountPassword,accountCity,accountState])
